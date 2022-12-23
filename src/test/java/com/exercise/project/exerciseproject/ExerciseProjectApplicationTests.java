234f90c7f51125105df6d511c66bb73efee82768
dariusz.kusak <dariusz.kusak@finalto.com>
2022-12-23 18:16:53 +0100

    Initial commit

---
+++ src/test/java/com/exercise/project/exerciseproject/ExerciseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.project.exerciseproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
