1e180f6225f3ceb4f6243e595e2a4e4e9134c632
dariusz.kusak <dariusz.kusak@finalto.com>
2022-12-27 16:14:23 +0100

    stack-exercises

---
+++ src/main/java/com/exercise/project/exerciseproject/stack/BracketDto.java
@@ -0,0 +1,11 @@
+package com.exercise.project.exerciseproject.stack;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class BracketDto {
+    private int index;
+    private char bracket;
+}
